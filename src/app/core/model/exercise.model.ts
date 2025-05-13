2e25a2a2a990544adca8cd2d0c4e75b323acf822
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-05-13 14:16:08 -0500

    feat: enhance display logic in routine and diet components

---
+++ src/app/core/model/exercise.model.ts
@@ -0,0 +1,6 @@
+export interface Exercise {
+  id: number;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+}
--- src/app/core/pages/diets-management/diets-management.component.ts
+++ src/app/core/pages/diets-management/diets-management.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Diet } from '../../model/diet.model';
 import { DietsService } from '../../services/diets.service';
-import {CommonModule, NgForOf, NgIf} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { DietDetailsDialogComponent } from '../../components/diet-details-dialog/diet-details-dialog.component';
+import { DietListComponent } from '../../components/diet-list/diet-list.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-diets-management',
   standalone: true,
-  templateUrl: './diets-management.component.html',
   imports: [
     FormsModule,
-    NgIf,
-    NgForOf
+    CommonModule,
+    MatDialogModule,
+    DietListComponent,
   ],
+  templateUrl: './diets-management.component.html',
   styleUrls: ['./diets-management.component.css']
 })
 export class DietsManagementComponent implements OnInit {
@@ -24,7 +28,7 @@ export class DietsManagementComponent implements OnInit {
     meals: []
   };
 
-  constructor(private dietsService: DietsService) {}
+  constructor(private dialog: MatDialog, private dietsService: DietsService) {}
 
   ngOnInit(): void {
     this.loadDiets();
@@ -50,5 +54,20 @@ export class DietsManagementComponent implements OnInit {
 
   assignCurrentDiet(diet: Diet): void {
     this.currentDiet = diet;
+  }
+
+  openDietDetails(dietId: number) {
+    this.dietsService.getDietById(dietId).subscribe(diet => {
+      const dialogRef = this.dialog.open(DietDetailsDialogComponent, {
+        width: '400px',
+        data: diet
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result?.assign) {
+          this.assignCurrentDiet(result.dietId); // tu lógica existente aquí
+        }
+      });
+    });
   }
 }
