a6834d2b1953554de359aa07b50cdf145936fc45
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-05-13 00:27:24 -0500

    feat: add profile view

---
+++ src/app/core/model/user-profile.response.ts
@@ -0,0 +1,9 @@
+export interface UserProfileResponse {
+  id: number;
+  fullName: string;
+  gender: string;
+  height: number;
+  weight: number;
+  activityLevelId: number;
+  goalId: number;
+}
--- src/app/core/pages/profile-management/profile-management.component.ts
+++ src/app/core/pages/profile-management/profile-management.component.ts
@@ -1,11 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { UserProfileService } from '../../services/user-profile.service';
+import { AuthService } from '../../../iam/services/auth.service';
+import { GoalResponse } from '../../model/goal.response';
+import { ActivityLevelResponse } from '../../model/activity-level.response';
+import { forkJoin, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {MatOption} from '@angular/material/core';
+import {MatSelect} from '@angular/material/select';
+import { CommonModule } from '@angular/common';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-profile-management',
-  imports: [],
+  standalone: true,
   templateUrl: './profile-management.component.html',
-  styleUrl: './profile-management.component.css'
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelect,
+    MatOption,
+    MatButtonModule,
+    MatInput,
+    CommonModule,
+  ],
+  styleUrls: ['./profile-management.component.css']
 })
-export class ProfileManagementComponent {
+export class ProfileManagementComponent implements OnInit {
+  form!: FormGroup;
+  goals: GoalResponse[] = [];
+  activityLevels: ActivityLevelResponse[] = [];
+  userId!: number;
+  isEdit = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private profileSvc: UserProfileService,
+    private auth: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.userId = Number(localStorage.getItem('userId'));
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      gender: ['', Validators.required],
+      height: [0, [Validators.required, Validators.min(1)]],
+      weight: [0, [Validators.required, Validators.min(1)]],
+      activityLevelId: [null, Validators.required],
+      goalId: [null, Validators.required],
+    });
+
+    // Cargar listas de selección
+    forkJoin({
+      goals: this.profileSvc.listGoals(),
+      levels: this.profileSvc.listActivityLevels(),
+      profile: this.profileSvc.getProfile(this.userId).pipe(catchError(() => of(null)))
+    }).subscribe(({ goals, levels, profile }) => {
+      this.goals = goals;
+      this.activityLevels = levels;
+      if (profile) {
+        this.isEdit = true;
+        // Mapear fullName a first/last
+        const [firstName, lastName] = profile.fullName.split(' ');
+        this.form.patchValue({ ...profile, firstName, lastName });
+      }
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+    const req = this.form.value;
+    const call = this.isEdit
+      ? this.profileSvc.updateProfile(this.userId, req)
+      : this.profileSvc.createProfile(this.userId, req);
+
+    call.subscribe(res => {
+      this.isEdit = true;
+      this.form.patchValue({
+        firstName: res.fullName.split(' ')[0],
+        lastName: res.fullName.split(' ')[1]
+      });
+      alert('Perfil guardado correctamente');
+    });
+  }
 }
