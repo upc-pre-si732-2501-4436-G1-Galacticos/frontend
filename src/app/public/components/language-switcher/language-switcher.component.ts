a81d05469ee28100b0025754593f9020a5d5543d
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-07-08 01:39:35 -0500

    feat: Add language switcher component

---
+++ src/app/public/components/language-switcher/language-switcher.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { TranslateService } from "@ngx-translate/core";
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-language-switcher',
+  standalone: true,
+  imports: [MatButtonToggleModule, NgFor],
+  templateUrl: './language-switcher.component.html',
+  styleUrl: './language-switcher.component.css'
+})
+export class LanguageSwitcherComponent {
+  currentLang: string = 'en';
+  languages: string[] = ['en', 'es'];
+
+  constructor(private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang || 'en';
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+    this.currentLang = language;
+  }
+}
